3f873b2d890a4c6d1eca235ae7088c17fa421443
Damian Dajer <damiandajer@gmail.com>
2016-04-07 21:26:36 +0200

    Dodanie zapisu zdjęcia w formacie jpeg w pamięci telefonu.

--- CheckPresence/app/src/main/java/com/app/checkpresence/CameraView.java
+++ CheckPresence/app/src/main/java/com/app/checkpresence/CameraView.java
@@ -5,12 +5,23 @@ package com.app.checkpresence;
  */
 
         import android.content.Context;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.graphics.Rect;
+        import android.graphics.YuvImage;
         import android.hardware.Camera;
+        import android.os.Environment;
         import android.util.Log;
         import android.view.SurfaceHolder;
         import android.view.SurfaceView;
+        import android.view.View;
+        import android.widget.Toast;
 
+        import java.io.File;
+        import java.io.FileInputStream;
+        import java.io.FileOutputStream;
         import java.io.IOException;
+        import java.nio.channels.FileChannel;
 
 public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
     private SurfaceHolder mHolder;
@@ -57,6 +68,21 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
         } catch (IOException e) {
             Log.d("ERROR", "Camera error on surfaceChanged " + e.getMessage());
         }
+
+
+        mCamera.setPreviewCallback(new Camera.PreviewCallback() {
+            public void onPreviewFrame(byte[] data, Camera _camera) {
+                //Log.d("surfaceChanged",String.format("Got %d bytes of camera data", _data.length));
+                //System.out.println("Got bytes of camera data: " + data.length);
+                //Bitmap previewBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                Camera.Parameters parameters = mCamera.getParameters();
+                Camera.Size size = parameters.getPreviewSize();
+
+                YuvImage image = new YuvImage(data, parameters.getPreviewFormat(),
+                        size.width, size.height, null);
+                addCopy(image);
+            }
+        });
     }
 
     @Override
@@ -65,5 +91,42 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
         //if you are unsing with more screens, please move this code your activity
         mCamera.stopPreview();
         mCamera.release();
+    }
+
+    public void addCopy(YuvImage image){
+        
+        FileOutputStream out = null;
+        File sd = Environment.getExternalStorageDirectory();
+        String backupDBPath = "backupBMP/backupImage.jpeg";
+        File backupImage = new File(sd, backupDBPath);
+        System.out.println(backupImage.toString());
+        if(!backupImage.exists()){
+            System.out.println("Tworzę backupDB");
+            try {
+                backupImage.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(backupImage.exists()) {
+            try {
+                out = new FileOutputStream(backupImage);
+                //image.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+                // PNG is a lossless format, the compression factor (100) is ignored
+                image.compressToJpeg(
+                        new Rect(0, 0, image.getWidth(), image.getHeight()), 90,
+                        out);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
